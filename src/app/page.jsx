5bd9d99e2cb5e63f254526ee481c22b5f622e831
Jacob Forsberg <jacob.forsbergg@gmail.com>
2024-04-20 21:48:57 +0200

    profile src

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,7 +15,7 @@ const Homepage = () => {
         {/* IMAGE CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 relative">
           <Image
-            src="/instagram.png"
+            src="/profile.png"
             alt="Image of jacob"
             fill
             className="object-contain"
